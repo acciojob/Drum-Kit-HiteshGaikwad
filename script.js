d964f030daaea6cf0ca98499192128476762a5fa
HiteshGaikwad <116432379+HiteshGaikwad@users.noreply.github.com>
2023-06-10 13:20:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.addEventListener("keydown", function(event) {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  const sound = key.querySelector(".sound");
+  
+  if (key) {
+    // Play the sound
+    sound.play();
+    
+    // Reset the sound if it is already playing
+    sound.currentTime = 0;
+    
+    // Add a class to highlight the pressed key
+    key.classList.add("playing");
+  }
+});
+
+document.addEventListener("keyup", function(event) {
